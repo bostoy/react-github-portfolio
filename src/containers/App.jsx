1f2299b88bdb63e506c7c3f74ddb62714efece15
Borislav Stoyanov <bostoycontact@gmail.com>
2021-02-25 11:22:54 +0200

    create global style / delete css

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, { Fragment } from 'react';
+import styled, { createGlobalStyle } from 'styled-components'
 import Header from '../components/Header/Header'
 import Profile from './Profile'
+
+const GlobalStyle = createGlobalStyle`
+ margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`
 
 const AppWrapper = styled.div`
 text-align:center;
@@ -9,10 +18,15 @@ text-align:center;
 
 function App() {
   return (
-    <AppWrapper >
-      <Header />
-      <Profile />
-    </AppWrapper>
+    <Fragment>
+      <GlobalStyle />
+      <AppWrapper >
+        <Header />
+        <Profile />
+      </AppWrapper>
+
+    </Fragment>
+
   );
 }
 
